3db01689ef6ebbffc25c2aa5369d7d9797891598
Samuel Flores <samuelflores@Samuels-Mac-mini.local>
2025-09-02 21:17:28 -0500

    add it network

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import clientInfo from "../config/clientInfo";
+import Hero from "components/Hero";
 
 function Home() {
   return (
     <div>
       <h1>Welcome to {clientInfo.businessName}</h1>
       <p>{clientInfo.tagline}</p>
+      <Hero />
     </div>
   );
 }
